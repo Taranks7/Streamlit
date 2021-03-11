fc15c00ac4f3e805db06ab0169cab59862123ca4
Taranjit Sehmbi <74196907+Taranks7@users.noreply.github.com>
2021-03-11 20:15:29 +0000

    corrected pyplot global issue
    Error: PyplotGlobalUseWarning: You are calling st.pyplot() without any arguments. After December 1st, 2020, we will remove the ability to do this as it requires the use of Matplotlib's global figure object, which is not thread-safe. To future-proof this code, you should pass in a figure as shown below:

--- sp500.py
+++ sp500.py
@@ -67,12 +67,13 @@ def price_plot(symbol):
   df = pd.DataFrame(data[symbol].Close)
   df['Date'] = df.index
   plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
+  fig, ax = plt.subplots()
   plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
   plt.xticks(rotation=90)
   plt.title(symbol, fontweight='bold')
   plt.xlabel('Date', fontweight='bold')
   plt.ylabel('Closing Price', fontweight='bold')
-  return st.pyplot()
+  return st.pyplot(fig)
 
 num_company = st.sidebar.slider('Number of Companies', 1, 5)
 
